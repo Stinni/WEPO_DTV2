7bff0c9f83f0fd9a8c695642d7b8f5d8452bd323
Stinni <kristinnf13@ru.is>
2016-02-03 00:29:21 +0000

    Nearly done, only the Angular app left

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,19 @@ module.exports = function ( grunt ) {
  grunt.loadNpmTasks('grunt-contrib-jshint');
  var taskConfig = {
    jshint: {
-     src: ['**/*.js'],
-     gruntfile: ['Gruntfile.js'],
+     src: ['src/**/*.js', '!fileToExclude.js'],
      options: {
+     	"curly":   true,
+     	"eqnull":  true,
+     	"eqeqeq":  true,
+     	"undef":   true,
+     	"globals": {
+     		"jQuery":  true,
+     		"console": true
+     	}
      }
    }
  };
  grunt.initConfig(taskConfig);
-}
+ grunt.registerTask('default', ['jshint']);
+};
